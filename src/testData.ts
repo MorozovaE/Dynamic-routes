dde4019b7c8f6d407ae0eab2c24998f07d9d0917
Liza Morozova <lizavetamorozova12@gmail.com>
2023-10-08 18:35:41 +0300

    data has been  changed

---
+++ src/testData.ts
@@ -0,0 +1,10 @@
+import { ICustomPage } from "./interfaces";
+
+export const previewPage: ICustomPage = {
+  path: "preview",
+  metaTitle: "Preview title ",
+  metaDescription: "Preview Description",
+  h1: "Preview Page",
+  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  backgroundColor: "#aaa",
+};

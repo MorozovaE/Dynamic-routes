c61937fa2c1c2351b71237d9b1cc01920a5e26d8
Liza Morozova <lizavetamorozova12@gmail.com>
2023-10-08 18:34:27 +0300

    added route to dynamic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import "./App.css";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
+import { useSelector } from "react-redux";
+import { pagesSelector } from "./store/features/pagesSlice";
+import { CustomPage } from "./pages/CustomPage/CustomPage";
 
 function App() {
+  let pages = useSelector(pagesSelector);
+
   return (
     <div className="App">
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<Navigate to="/home" />} />
+        {pages.map((el, idx) => {
+          return (
+            <Route
+              key={idx}
+              path={`/${el.path}`}
+              element={<CustomPage data={el} />}
+            />
+          );
+        })}
       </Routes>
     </div>
   );
